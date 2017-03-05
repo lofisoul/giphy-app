f8a505663ceab283940bdbb772cd4fc70a4bfb5a
lofisoul <jason.nolfi@gmail.com>
2017-03-04 21:11:59 -0600

    Updated AJAX call to https

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function initGif() {
 
 function callChar(term) {
   $('.fish-bowl').empty();
-  var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + term + '+' + keyTerm + '&api_key=dc6zaTOxFJmzC&limit=10';
+  var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + term + '+' + keyTerm + '&api_key=dc6zaTOxFJmzC&limit=10';
   console.log(queryURL);
   $.ajax({
       url: queryURL,
